11495c66040709210d9d33801eadb113ccffb358
laccces <en07djc@icloud.com>
2023-06-08 14:29:13 +0100

    Added venv, installed flask, started connecting frontend to backend

---
+++ flask-app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def get_data():
+    data = {'message': 'Hello, World!'}
+    return jsonify(data), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
